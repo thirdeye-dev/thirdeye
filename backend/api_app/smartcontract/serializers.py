9040532205b5baad3e95c70ed8683eff2b103384
0x0elliot <wolfwithahat@protonmail.com>
2023-02-05 16:48:32 +0530

    Adding basic address validation in smart contract CRUD APIs

--- backend/api_app/smartcontract/serializers.py
+++ backend/api_app/smartcontract/serializers.py
@@ -1,7 +1,17 @@
+import re
+
 from rest_framework import serializers
 from .models import SmartContract
 
+def smart_contract_validator(value):
+    pattern = re.compile(r'^0x[a-fA-F0-9]{40}$')
+    if not pattern.match(value):
+        raise serializers.ValidationError("Invalid smart contract address")
+
+
 class SmartContractSerializer(serializers.ModelSerializer):
+    address = serializers.CharField(validators=[smart_contract_validator])
+
     class Meta:
         model = SmartContract
         fields = '__all__'
