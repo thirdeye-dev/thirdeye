cb0a022f7e51b845043c34040fca98bd60b95962
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-03-16 13:52:37 +0530

    Fixing some useless code

--- backend/authentication/org/serializers.py
+++ backend/authentication/org/serializers.py
@@ -1,14 +1,8 @@
 from django.core.validators import EmailValidator
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
-from organizations.models import OrganizationInvitation, Organization
+from organizations.models import Organization
 
-from django.db import models
-
-# create ROLE_CHOICES 
-class ROLE_CHOICES(models.TextChoices):
-    MEMBER = 'member'
-    ADMIN = 'admin'
 
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,15 +15,11 @@ class UserInviteSerializer(serializers.Serializer):
         validators=[EmailValidator(message=_('Enter a valid email address.'))],
         help_text=_('Email address of the user to be invited.'),
     )
-    role = serializers.ChoiceField(
-        choices=ROLE_CHOICES.choices,
-        help_text=_('Role of the invited user.'),
-    )
 
     def save(self, **kwargs):
         request = self.context['request']
         organization = kwargs.get('organization')
         email = self.validated_data['email']
-        role = self.validated_data['role']
-        invite = organization.invite_user(request.user, email, role)
+
+        invite = organization.invite_user(request.user, email)
         return invite
