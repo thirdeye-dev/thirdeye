34a2e8850f59f4657cc3dd70d8fa4eac0e162753
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-03-21 11:19:22 +0530

    Fixes #28

--- backend/api_app/smartcontract/views.py
+++ backend/api_app/smartcontract/views.py
@@ -1,44 +1,24 @@
-from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import permissions, viewsets
 from .models import SmartContract
 from .serializers import SmartContractSerializer
-
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
 
-
-class SmartContractList(generics.ListAPIView):
-    queryset = SmartContract.objects.all()
+# add permissions later
+class SmartContractViewSet(viewsets.ModelViewSet):
     serializer_class = SmartContractSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [permissions.IsAuthenticated]
+        else:
+            permission_classes = [IsOwner]
+        return [permission() for permission in permission_classes]
 
     def get_queryset(self):
-        return self.queryset.filter(owner=self.request.user)
+        return SmartContract.objects.filter(owner=self.request.user)
 
-class SmartContractCreate(generics.CreateAPIView):
-    queryset = SmartContract.objects.all()
-    serializer_class = SmartContractSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class SmartContractRetrieve(generics.RetrieveAPIView):
-    queryset = SmartContract.objects.all()
-    serializer_class = SmartContractSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        return self.queryset.filter(owner=self.request.user)
-
-
-class SmartContractUpdate(generics.UpdateAPIView):
-    queryset = SmartContract.objects.all()
-    serializer_class = SmartContractSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwner]
-
-
-class SmartContractDelete(generics.DestroyAPIView):
-    queryset = SmartContract.objects.all()
-    serializer_class = SmartContractSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
