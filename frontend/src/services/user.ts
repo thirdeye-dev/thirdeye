d1b652b90805c7d5155dfdec2afd55b8773fcb8e
Syed Ahkam <smahkam57@gmail.com>
2023-04-07 01:44:38 +0530

    clean: redundant TODO expr

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -1,7 +1,6 @@
 import { User } from "@/models/user";
 import axios from "@/axios";
 
-// TODO: Ideally we should get a single object back, instead of an array with a single child.
 export async function fetchCurrentUser(): Promise<User | null> {
   try {
     const response = await axios.get<User>("/authentication/me");
