49415604a61af132dc408ff43322602bc0ee5e97
Syed Ahkam <smahkam57@gmail.com>
2023-04-12 04:30:00 +0530

    fix: convert user model to use an interface

--- frontend/src/models/user.ts
+++ frontend/src/models/user.ts
@@ -1,6 +1,6 @@
-import { Organization } from "./organization";
+import Organization from "./organization";
 
-export class User {
+export default interface User {
   id: number;
   email: string;
   username: string;
@@ -9,24 +9,4 @@ export class User {
   avatar: string;
   is_staff: boolean;
   organizations: Array<Organization>;
-
-  constructor(
-    id: number,
-    email: string,
-    username: string,
-    created_at: string,
-    updated_at: string,
-    avatar: string,
-    is_staff: boolean,
-    organizations: Array<Organization>
-  ) {
-    this.id = id;
-    this.email = email;
-    this.username = username;
-    this.created_at = created_at;
-    this.updated_at = updated_at;
-    this.avatar = avatar;
-    this.is_staff = is_staff;
-    this.organizations = organizations;
-  }
 }
