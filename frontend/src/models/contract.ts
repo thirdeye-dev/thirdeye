5cf32a9193b1d8edefd336db8039929b857c3455
Syed Ahkam <smahkam57@gmail.com>
2023-04-12 04:12:30 +0530

    fix: model types should be interfaces; not classes
    An interface perfectly suffices for the functionality we need right now, no need to overcomplicate with class instances. Not to mention, we weren't even instantiating instances on Se/De

--- frontend/src/models/contract.ts
+++ frontend/src/models/contract.ts
@@ -1,21 +1,7 @@
-export default class Contract {
+export default interface Contract {
   id: number;
   name: string;
   address: string;
   chain: string;
   network: string;
-
-  constructor(
-    id: number,
-    name: string,
-    address: string,
-    chain: string,
-    network: string
-  ) {
-    this.id = id;
-    this.name = name;
-    this.address = address;
-    this.chain = chain;
-    this.network = network;
-  }
 }
