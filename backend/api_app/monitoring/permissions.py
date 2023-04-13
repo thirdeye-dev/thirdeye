06e235db08c562e541e2d3f9eca139115ffee914
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-04-13 22:00:27 +0530

    Fixing monitoring endpoints

---
+++ backend/api_app/monitoring/permissions.py
@@ -0,0 +1,30 @@
+from rest_framework import permissions
+from .models import Alerts
+
+from authentication.organizations.models import Membership
+
+class AlertCanBeAccessed(permissions.BasePermission):
+    """
+    Custom permission to only allow members of an organization to access alerts.
+    """
+
+    message = (
+        "You are not a member of the organization. "
+        "Try checking owner_organization is right."
+    )
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        
+        alert = Alerts.objects.filter(id=view.kwargs['pk']).first()
+        if alert is None:
+            self.message = (
+                f"Alert with id {view.kwargs['pk']} does not exist."
+            )
+            return False
+
+        return Membership.is_member(
+            user=request.user,
+            organization=alert.owner_organization,
+        )
--- backend/api_app/monitoring/views.py
+++ backend/api_app/monitoring/views.py
@@ -3,6 +3,11 @@ import logging
 from rest_framework import viewsets
 
 from api_app.smartcontract.permissions import CanAccessSmartContract
+from authentication.organizations.permissions import IsMember
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from .models import Alerts
 from .serializers import AlertsAPISerializer
@@ -10,11 +15,29 @@ from .serializers import AlertsAPISerializer
 logger = logging.getLogger(__name__)
 
 
-class AlertViewSet(viewsets.ModelViewSet):
+class SmartContractAlertListViewSet(ListAPIView):
     serializer_class = AlertsAPISerializer
     permission_classes = [CanAccessSmartContract]
 
     def get_queryset(self):
         smart_contract = self.request.query_params.get("smart_contract")
         queryset = Alerts.objects.filter(smart_contract=smart_contract)
+        return queryset
+
+class OrganizationAlertListViewSet(ListAPIView):
+    serializer_class = AlertsAPISerializer
+    permission_classes = [IsMember]
+
+    def get_queryset(self):
+        organization = self.request.query_params.get("owner_organization")
+        queryset = Alerts.objects.filter(organization=organization)
+        return queryset
+
+
+class AlertRetrieveAPIView(RetrieveAPIView):
+    serializer_class = AlertsAPISerializer
+    permission_classes = [IsMember]
+
+    def get_queryset(self):
+        queryset = Alerts.objects.all()
         return queryset
