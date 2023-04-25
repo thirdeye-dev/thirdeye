e4bff799db40dfcd8f374c1e368976134678ad79
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-04-25 20:06:44 +0530

    Removing deploy.sh for now

--- docker/deploy.sh
+++
@@ -1,8 +0,0 @@
-docker login -u ${ACTOR} -p ${TOKEN} ghcr.io && 
-cd /home/deployuser/thirdeye/docker &&
-echo "pulling latest docker images" >> logs.txt &&
-docker pull ghcr.io/thirdeye-dev/thirdeye:latest && 
-# save echo logs to logs.txt
-echo "shutting down docker compose services at ${CODE_PATH}" > logs.txt &&
-docker-compose down && 
-docker-compose -f ${CODE_PATH}/docker/demo.yml up -d
