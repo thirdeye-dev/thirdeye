f1fbc821ed49b4b2086bbc928fbe9f5a607d9fd4
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-04-25 19:55:00 +0530

    Changing bash file sequence

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,6 +1,7 @@
 docker login -u ${{ github.actor }} -p ${{ secrets.GITHUB_TOKEN }} ghcr.io && 
-docker pull ghcr.io/thirdeye-dev/thirdeye:latest && 
 cd /home/deployuser/thirdeye/docker &&
+echo "pulling latest docker images" >> logs.txt &&
+docker pull ghcr.io/thirdeye-dev/thirdeye:latest && 
 # save echo logs to logs.txt
 echo "shutting down docker compose services at ${CODE_PATH}" > logs.txt &&
 docker-compose down && 
