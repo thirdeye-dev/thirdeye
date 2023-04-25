f61e465474369fd22bf0d0a394a050d3183db927
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-04-25 19:22:20 +0530

    Adding loggin to CI/Cd

--- docker/cicd.sh
+++ docker/cicd.sh
@@ -1,5 +1,7 @@
 docker login -u ${{ github.actor }} -p ${{ secrets.GITHUB_TOKEN }} ghcr.io && 
 docker pull ghcr.io/thirdeye-dev/thirdeye:latest && 
 cd ${CODE_PATH}/docker &&
+# save echo logs to logs.txt
+echo "shutting down docker compose services at ${CODE_PATH}" > logs.txt &&
 docker-compose down && 
 docker-compose -f ${CODE_PATH}/docker/demo.yml up -d
