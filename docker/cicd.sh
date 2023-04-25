65509ddbefc852ac9ed594061317bbe4a48e05cf
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-04-25 19:08:40 +0530

    Fixing CI/CD errors

--- docker/cicd.sh
+++ docker/cicd.sh
@@ -1,4 +1,5 @@
 docker login -u ${{ github.actor }} -p ${{ secrets.GITHUB_TOKEN }} ghcr.io && 
 docker pull ghcr.io/thirdeye-dev/thirdeye:latest && 
+cd ${CODE_PATH}/docker &&
 docker-compose down && 
 docker-compose -f ${CODE_PATH}/docker/demo.yml up -d
