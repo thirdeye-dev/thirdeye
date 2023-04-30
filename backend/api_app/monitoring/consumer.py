5f356cf41d8e6ce1b8b451f1d45d7c514cabeea8
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-04-30 23:22:42 +0530

    fix: empty notifications (#80)

--- backend/api_app/monitoring/consumer.py
+++ backend/api_app/monitoring/consumer.py
@@ -1,27 +1,25 @@
-import json
-import logging
 import os
-
 import jwt
-from channels.db import database_sync_to_async
-from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+import redis
+import logging
+
 from django.conf import settings
+from authentication.models import User
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
 from django.contrib.auth.models import AnonymousUser
 from django.core.exceptions import ObjectDoesNotExist
-
-from authentication.models import User
 from authentication.organizations.models import Membership, Organization
 
 logger = logging.getLogger(__name__)
-
 
 class NotificationsConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.path_ = os.path.join(settings.BASE_DIR, "configuration", "log.txt")
 
-        self.owner_organization_id = self.scope["url_route"]["kwargs"][
-            "owner_organization_id"
-        ]
+        self.owner_organization_id = self.scope['url_route']['kwargs']['owner_organization_id']
         self.user = await self.get_user()
 
         if not self.user.is_authenticated:
@@ -30,42 +28,45 @@ class NotificationsConsumer(AsyncWebsocketConsumer):
             await self.close()
         else:
             await self.channel_layer.group_add(
-                self.owner_organization_id, self.channel_name
+                self.owner_organization_id,
+                self.channel_name
             )
             await self.accept()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
-            self.owner_organization_id, self.channel_name
+            self.owner_organization_id,
+            self.channel_name
         )
 
     async def receive(self, text_data):
         pass
 
     async def send_notification(self, event):
-        notification = event["notification"]
+        notification = event['notification']
 
         await self.send(text_data=json.dumps(notification))
 
     @database_sync_to_async
     def get_user(self):
         try:
-            query_string = self.scope["query_string"].decode().split("&")
+            query_string = self.scope['query_string'].decode().split('&')
 
-            query = {i.split("=")[0]: i.split("=")[1] for i in query_string}
+            query = {
+                i.split("=")[0]:i.split("=")[1] 
+                for i in query_string
+            }
 
             token_key = query.get("token")
 
             if not token_key:
                 return AnonymousUser()
-            decoded_token = jwt.decode(
-                token_key, settings.SECRET_KEY, algorithms=["HS256"]
-            )
+            decoded_token = jwt.decode(token_key, settings.SECRET_KEY, algorithms=['HS256'])
 
-            user_id = decoded_token.get("user_id")
+            user_id = decoded_token.get('user_id')
             if user_id:
                 return User.objects.get(id=user_id)
-
+            
             return AnonymousUser()
         except (AttributeError, IndexError, ObjectDoesNotExist):
             return AnonymousUser()
@@ -74,3 +75,5 @@ class NotificationsConsumer(AsyncWebsocketConsumer):
     def is_member_of_organization(self):
         org = Organization.objects.get(id=self.owner_organization_id)
         return Membership.is_member(user=self.user, organization=org)
+
+
