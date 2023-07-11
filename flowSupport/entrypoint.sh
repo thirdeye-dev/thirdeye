dd8a2d30e3b58aad56b90ac4fd0d376d3914ae12
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-07-12 02:01:56 +0530

    Updated flowSupport

--- flowSupport/entrypoint.sh
+++ flowSupport/entrypoint.sh
@@ -11,3 +11,5 @@ exec ls
 # [ -f /app/main ] && echo "File /app/main has been created" || echo "Error: File /app/main does not exist" | curl -X POST -d @- https://eo8gqd1da82iqn8.m.pipedream.net
 
 echo "Starting FLOW service"
+
+
