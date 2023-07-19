1607ded4bddd9c1881151a25b297eb0249307d30
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-07-19 15:12:52 +0530

    fix: temp updation of the default BACKEND_URL

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,5 @@
 // ENV variables
-export const BACKEND_URL = process.env.BACKEND_URL || "localhost:8000";
+export const BACKEND_URL = process.env.BACKEND_URL || "139.59.90.152:8000";
 
 // UI behaviour
 export const DEFAULT_ACTIVE_DASHBOARD_TAB = "overview";
