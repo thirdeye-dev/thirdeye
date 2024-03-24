db24d91ae454f0cc4d5514973c363dcadbbbfe81
Aditya <60684641+0x0elliot@users.noreply.github.com>
2024-03-24 17:20:58 +0530

    fix: ts fixes

--- frontend/src/components/ChainIcon.tsx
+++ frontend/src/components/ChainIcon.tsx
@@ -7,13 +7,14 @@ export default function ChainIcon({ chain }: { chain: Chain }) {
 
   switch (chain) {
     case Chain.ETH:
+      // @ts-ignore
       return <FaEthereum />;
 
     case Chain.SOL:
+      // @ts-ignore
       return <Image src="https://cryptologos.cc/logos/solana-sol-logo.svg" alt="Solana" width={12} height={12} />;
-    case Chain.ARB:
-      return <Image src="https://cryptologos.cc/logos/arbitrum-arb-logo.svg" alt="Arbitrum" width={24} height={24} />;
     default:
+      // @ts-ignore
       return <FaEthereum />;
   }
 }
