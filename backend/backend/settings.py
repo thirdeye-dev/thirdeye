5890c7273e700bd33cdc4663b5253a608f560c7b
Aditya <60684641+0x0elliot@users.noreply.github.com>
2024-03-26 02:21:52 +0530

    fix: removing postgres from settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -200,15 +200,13 @@ DATABASES = {
     }
 }
 
-
-if DEMO_INSTANCE:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-
+# if DEMO_INSTANCE:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
